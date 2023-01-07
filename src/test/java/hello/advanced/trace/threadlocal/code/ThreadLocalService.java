ffb12ce3aa6daee425de16684dd416d4608d4096
yujinlee <now8567@hanmail.net>
2023-01-07 21:55:55 +0900

    ThreadLocal - ThreadLocal 활용

--- src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
+++ src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -3,16 +3,16 @@ package hello.advanced.trace.threadlocal.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FieldService {
+public class ThreadLocalService {
 
-    private String nameStore;
+    private ThreadLocal<String> nameStore = new ThreadLocal<>();
 
     public String logic(String name){
-        log.info("store name = {} -> nameStore = {}", name, nameStore);
-        nameStore = name;
+        log.info("store name = {} -> nameStore = {}", name, nameStore.get());
+        nameStore.set(name);
         sleep(1000);
-        log.info("search nameStore = {}",nameStore);
-        return nameStore;
+        log.info("search nameStore = {}",nameStore.get());
+        return nameStore.get();
     }
 
     private void sleep(int millis) {
