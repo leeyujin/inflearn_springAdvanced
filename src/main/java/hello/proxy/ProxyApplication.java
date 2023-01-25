62d49ad4cd3eefb41f4f0142345b6ab6c845a73c
yujinlee <now8567@hanmail.net>
2023-01-25 22:09:13 +0900

    구체 클래스 기반 프록시

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import({ConcreteProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의  hello.proxy.config 위치 설정파일이 자동으로 빈으로 등록되지않게끔 하기위해 제한. (다양한 버전의 config들이 추가될 예정이기때문)
 public class ProxyApplication {
 
